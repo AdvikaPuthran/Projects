2d8b2914615977a4207ac12e0002a2097122944f
AdvikaPuthran <138208645+AdvikaPuthran@users.noreply.github.com>
2023-09-29 14:32:04 +0530

    Add files via upload

---
+++ Sprint3(Spring Boot)/sb/entity/Employee.java
@@ -0,0 +1,49 @@
+package com.employee.sb.entity;
+
+import com.employee.sb.entity.Employee;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="employees")
+public class Employee {
+
+	@Id
+	private int eid;
+	
+	@Column(length=25, nullable= true)
+	@NotBlank(message="Employee name cannot be blank")
+	private String first_name;
+	
+	@Column(length=25, nullable= true)
+	@NotBlank(message="Employee Surname cannot be blank")
+	private String last_name;
+	
+	@Column(length=20, nullable= true, unique=true)
+	@NotBlank(message="Employee Email ID cannot be blank")
+	@Email(message="Email ID is not proper")
+	private String email;
+	
+	@Column(length=11, nullable= true, unique=true)
+	@NotNull(message="Phone number cannot be null")
+	private long ephone;
+	
+	@Column(length=25, nullable= true, unique=true)
+	@NotBlank(message="Designation cannot be blank")
+	private String designation;
+
+	@ManyToOne()
+	@JoinColumn(name="did", referencedColumnName="did" )
+	Department department;
+}
