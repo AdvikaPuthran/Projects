ef319dba764c6b8cd050c3696449caafb16da89b
AdvikaPuthran <138208645+AdvikaPuthran@users.noreply.github.com>
2023-09-29 14:33:00 +0530

    Add files via upload

---
+++ Sprint3(Spring Boot)/sb/exception/DepartmentIdNotFoundException.java
@@ -0,0 +1,14 @@
+package com.employee.sb.exception;
+
+public class DepartmentIdNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public DepartmentIdNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
+
