cf09c23fce5bd3eb794854e030d6a23ae762dd95
AdvikaPuthran <138208645+AdvikaPuthran@users.noreply.github.com>
2023-10-01 22:06:10 +0530

    Add files via upload

---
+++ Sprint3(Spring Boot)/sb/serviceimpl/DepartmentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.employee.sb.serviceimpl;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.employee.sb.entity.Department;
+import com.employee.sb.exception.DepartmentIdNotFoundException;
+import com.employee.sb.repository.DepartmentRepository;
+import com.employee.sb.service.DepartmentService;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+	
+	@Autowired
+	DepartmentRepository drepo;
+
+	@Override
+	public Department addDepartment(Department department) {
+		
+		return drepo.save(department);
+	}
+	
+	@Override
+	public Department getDeptDetail(int did) {
+		return drepo.findById(did).orElseThrow(()-> new DepartmentIdNotFoundException("Incorrect Department id"));
+	}
+
+	@Override
+	public Department updateDeptDetail(Department department, int did) {
+		Department newDept = drepo.findById(did).orElseThrow(()-> new DepartmentIdNotFoundException("Incorrect Department id"));
+		
+		newDept.setNoOfEmpl(department.getNoOfEmpl());
+		newDept.setDeptHOD(department.getDeptHOD());
+		
+		drepo.save(newDept);
+		return newDept;
+	}
+
+	@Override
+	public List<Department> getAllDept() {
+	
+		return drepo.findAll();
+	}
+
+}
+
